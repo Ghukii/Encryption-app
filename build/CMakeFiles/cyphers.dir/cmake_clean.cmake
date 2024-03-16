3cf384dab999a8a902e687a65abb0a5ea0cd22ec
Ghukii <68664943+Ghukii@users.noreply.github.com>
2024-03-16 14:05:59 +0300

    Add files via upload

---
+++ build/CMakeFiles/cyphers.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cyphers.dir/cyphers.cpp.o"
+  "CMakeFiles/cyphers.dir/cyphers.cpp.o.d"
+  "cyphers.cpython-311-x86_64-linux-gnu.so"
+  "cyphers.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cyphers.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
